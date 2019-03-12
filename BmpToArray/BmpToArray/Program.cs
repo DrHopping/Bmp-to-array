ec5016af489a14de3cb69a8bfaac7313b5498db0
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-12 21:26:44 +0200

    Only for 1bit bmp images

--- BmpToArray/BmpToArray/Program.cs
+++ BmpToArray/BmpToArray/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace BmpToArray
 {
@@ -10,7 +13,67 @@ namespace BmpToArray
     {
         static void Main(string[] args)
         {
-            
+            Bitmap bmp = new Bitmap("image.bmp");
+            int[,] image = BmpTo2D(bmp);
+            FileCheck();
+            Save(ArrayToFormatString(image), "Array.txt");
         }
+
+        static void Save(string str, string filePath)
+        {
+            StreamWriter sw = new StreamWriter(filePath);
+            sw.WriteLine(str);
+            sw.Close();
+        }
+
+        static void FileCheck()
+        {
+            if (!File.Exists("Array.txt"))
+                File.Create("Array.txt").Dispose();
+        }
+
+        static string ArrayToFormatString(int[,] array)
+        {
+            string arrayStr = "{";
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                arrayStr += "{";
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j < array.GetLength(1) - 1)
+                        arrayStr += array[i, j] + ",";
+                    else
+                        arrayStr += array[i, j];
+
+                }
+
+                if (i < array.GetLength(0) - 1)
+                    arrayStr += "},";
+                else
+                    arrayStr += "}";
+            }
+            arrayStr += "}";
+
+            return arrayStr;
+        }
+
+        static int[,] BmpTo2D(Bitmap bmp)
+        {
+            int[,] array = new int[bmp.Height, bmp.Width];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (bmp.GetPixel(j, i).B == 0)
+                        array[i, j] = 0;
+                    else
+                        array[i, j] = 1;
+                }
+            }
+            return array;
+        }
+
     }
 }
